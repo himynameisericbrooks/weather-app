6b1b42069094de24a0590e002ef179db21a93ba9
Eric Brooks <ericbrookspro@gmail.com>
2020-09-08 23:47:53 -0500

    Printing weather results to the console

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=56bbc6f76fa91121aae7a4a8ab3b0466&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=56bbc6f76fa91121aae7a4a8ab3b0466&query=37.8267,-122.4233&units=f'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
 })
