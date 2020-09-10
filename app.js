b23c2d64f96788255a7b6e5e6ff65ccaf092e3ab
Eric Brooks <ericbrookspro@gmail.com>
2020-09-10 00:06:58 -0500

    Error handling for Mapbox

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ request({ url: url, json: true }, (error, response) => {
     }
 })
 
-// Geocoding
-// Address - Lat/Long -> Weather
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGlteW5hbWVpc2VyaWNicm9va3MiLCJhIjoiY2tld2JqemZzMm8yMDJzcG5uZWRrNTVnNyJ9.wl7UoQXjVU8fqI3yltejww&limit=1'
 
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGlteW5hbWVpc2VyaWNicm9va3MiLCJhIjoiY2tld2JqemZzMm8yMDJzcG5uZWRrNTVnNyJ9.wl7UoQXjVU8fqI3yltejww&limit=1'
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     const latitude = response.body.features[0].center[1]
-//     const longitude = response.body.features[0].center[0]
-//     console.log(latitude, longitude)
-// })
+request({ url: geocodeURL, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location services')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
+})
