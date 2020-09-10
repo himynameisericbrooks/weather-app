3d91f8eccbf41b2024f638f323f958e59af37033
Eric Brooks <ericbrookspro@gmail.com>
2020-09-09 23:50:30 -0500

    mapbox api added

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const url = 'http://api.weatherstack.com/current?access_key=56bbc6f76fa91121aae7
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
 })
+
+// Geocoding
+// Address - Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGlteW5hbWVpc2VyaWNicm9va3MiLCJhIjoiY2tld2JqemZzMm8yMDJzcG5uZWRrNTVnNyJ9.wl7UoQXjVU8fqI3yltejww&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
+})
