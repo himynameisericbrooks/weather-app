9560c924c9e669466eb41e33e74b0de5f69f718f
Eric Brooks <ericbrookspro@gmail.com>
2020-09-17 00:02:34 -0500

    callback chaining complete

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const address = process.argv[2]
 
 if (!address) {
     console.log('Please provide an address' )
-}
-console.log(process.argv)
-
-geocode(address, (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log(error)
         }
-
-        console.log(data.location)
-        console.log(forecastData)
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+}
+console.log(process.argv)
 
